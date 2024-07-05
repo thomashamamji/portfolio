2923896d1e4ad3b78ad07090f672a8f587adc0ad
thomashamamji <thomas.hamamji@gmail.com>
2024-07-05 22:06:01 +0200

    Changed linkedin link.

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -100,7 +100,7 @@ function Home2() {
                             </li>
                             <li className="social-icons">
                                 <a
-                                    href="https://www.linkedin.com/in/soumyajit4419/"
+                                    href="https://www.linkedin.com/in/thomashamamji/"
                                     target="_blank"
                                     rel="noreferrer"
                                     className="icon-colour  home-social-icons"
